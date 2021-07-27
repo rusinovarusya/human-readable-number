f158db6d4a154daa725699a077ea2202d3eeff12
Irina Rusinova <rusinovarusya@mail.ru>
2021-07-27 17:50:40 +0300

    feat: delete work of thousands

--- src/index.js
+++ src/index.js
@@ -6,86 +6,39 @@ module.exports = function toReadable (number) {
         10: "ten", 11: "eleven", 12: "twelve",
         13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen",
         20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety",
-        100: "hundred", 1000: "thousand"
+        100: "hundred"
     };
 
 
     let getUnits = (n) => Math.floor(n % 10);
     let getDozens = (n) => Math.floor(n / 10) % 10;
     let getHundreds = (n) => Math.floor(n / 100) % 10;
-    let getThousand = (n) => Math.floor(n / 1000) % 10;
 
-
-    let t = getThousand(num);
-    let h = getHundreds(num);
-    let d = getDozens(num);
     let u = getUnits(num);
+    let d = getDozens(num);
+    let h = getHundreds(num);
+    
 
-    if (!t && !h && !d && !u) return "zero";
-    if (!t && !h && !d && u) return `${words[u]}`;
-    if (!t && !h && d && !u) return `${words[d * 10]}`;
-    if (!t && !h && d && u) {
+    if (!h && !d && !u) return "zero";
+    if (!h && !d && u) return `${words[u]}`;
+    if (!h && d && !u) return `${words[d * 10]}`;
+    if (!h && d && u) {
         if (d == 1)`${words[d * 10 + u]}`
         else return `${words[d * 10]} ${words[u]}`;
     }
 
-    if (!t && h && !d && !u) {
+    if (h && !d && !u) {
         return `${words[h]} hundred `;
     }
-    if (!t && h && !d && u) {
+    if (h && !d && u) {
         return `${words[h]} hundred ${words[u]}`;
     }
-    if (!t && h && d && !u) {
+    if (h && d && !u) {
         return `${words[h]} hundred ${words[d * 10]}`;
     }
-    if (!t && h && d && u) {
+    if (h && d && u) {
         if (d == 1)`${words[h]} hundred ${words[d * 10 + u]}`;
         else return `${words[h]} hundred ${words[d * 10]} ${words[u]}`;
-    }
-
-    if (t && !h && !d && !u) {
-        if (t == 1) return `${t} thousand`;
-        else return `${t} thousands`;
-    }
-    if (t && !h && !d && u) {
-        if (t == 1) return `${words[t]} thousand ${words[u]}`;
-        else return `${words[t]} thousands ${words[u]}`;
-    }
-    if (t && !h && d && !u) {
-        if (t == 1) return `${words[t]} thousand ${words[d * 10]}`;
-        else return `${words[t]} thousands ${words[d * 10]}`;
-    }
-    if (t && !h && d && u) {
-        if (t == 1) {
-            if (d == 1) return `${words[t]} thousand ${words[d * 10 + u]}`;
-            else return `${words[t]} thousand ${words[d * 10]} ${words[u]}`;
-        }
-        else {
-            if (d == 1) return `${words[t]} thousands ${words[d * 10 + u]}`;
-            else return `${words[t]} thousands ${words[d * 10]} ${words[u]}`;
-        }
-    }
-
-    if (t && h && !d && !u) {
-        if (t == 1) return `${words[t]} thousand ${words[h]} hundred`;
-        else return `${words[t]} thousands ${words[h]} hundred`;
-    }
-    if (t && h && !d && u) {
-        if (t == 1) return `${words[t]} thousand ${words[h]} hundred ${words[u]}`;
-        else return `${words[t]} thousands ${words[h]} hundred ${words[u]}`;
-    }
-    if (t && h && d && !u) {
-        if (t == 1) return `${words[t]} thousand ${words[h]} hundred ${words[d * 10]}`;
-        else return `${words[t]} thousands ${words[h]} hundred ${words[d * 10]}`;
-    }
-    if (t && h && d && u) {
-        if (t == 1) {
-            if (d == 1) return `${words[t]} thousand ${words[h]} hundred ${words[d * 10 + u]}`;
-            else return `${words[t]} thousand ${words[h]} hundred ${words[d * 10]} ${words[u]}`;
-        } else {
-            if (d == 1) return `${words[t]} thousands ${words[h]} hundred ${words[d * 10 + u]}`;
-            else return `${words[t]} thousands ${words[h]} hundred ${words[d * 10]} ${words[u]}`;
-        }
     }
 
 }
